fd687447cd1b8d04a993126c12ff6f817a4f6de2
Krzysztof Kaczor <chris@kaczor.io>
2018-11-04 22:14:56 +0100

    add missing typings for link

--- index.d.ts
+++ index.d.ts
@@ -59,6 +59,8 @@ declare namespace Truffle {
   }
 
   interface Deployer {
+    link(library: Truffle.Contract<any>, destination: Truffle.Contract<any>): Deployer;
+    link(library: Truffle.Contract<any>, destinations: Array<Truffle.Contract<any>>): Deployer;
     deploy<T extends any[]>(c: ContractNew<T>, ...args: T): Deployer;
   }
 
